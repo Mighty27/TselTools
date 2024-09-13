865f26163871bd16a50ef6589b64e9a9185015a7
mighty27 <81411396+Mighty27@users.noreply.github.com>
2024-09-13 08:04:19 +0700

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,12 +1,17 @@
 var inputNumber = document.getElementById("input-myid");
 const submitButton = document.getElementById("capture-button");
 const viewIndihome = document.getElementById("url-indihome");
+const viewModal = document.getElementById("modalData");
 const button = document.getElementsByClassName("capture-button");
+const Name = document.getElementsByClassName("header-name-modal");
+
 
 submitButton.addEventListener("click", function() {
-    var data = inputNumber.value;
-    const link = "https://indihome.co.id/tracking-order?trackid=MYID-"
-    const linkTo = link + data;
-    console.log(linkTo);
-    viewIndihome.innerHTML = "<iframe height='480px' src="+ linkTo +" frameborder='0'></iframe>";
-  });
+  console.log(viewModal.style);
+  var data = inputNumber.value;
+  const infoMYID = "Indihome MYID-";
+  const link = "https://indihome.co.id/tracking-order?trackid=MYID-";
+  const linkTo = link + data;
+  document.getElementById('staticBackdropLabel').innerHTML = infoMYID + data;
+  viewIndihome.innerHTML = "<iframe height='520px' width='100%' src="+ linkTo +" frameborder='0'></iframe>";
+});
